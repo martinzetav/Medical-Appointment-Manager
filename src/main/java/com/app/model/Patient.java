9a469f3f8c9d59682708d036f21329ed5629fc72
Martin Zalazar <martinzetav@gmail.com>
2025-02-27 08:28:38 -0300

    se creo clase Patient

---
+++ src/main/java/com/app/model/Patient.java
@@ -0,0 +1,25 @@
+package com.app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "patients")
+public class Patient extends Person {
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "contact_id", referencedColumnName = "id")
+    private Contact contact;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Appointment> appointments = new HashSet<>();
+}
