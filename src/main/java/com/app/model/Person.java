f341a4e806b189c6139118c481ad777ee6511510
Martin Zalazar <martinzetav@gmail.com>
2025-02-27 08:27:17 -0300

    se crea clase abstracta Person

---
+++ src/main/java/com/app/model/Person.java
@@ -0,0 +1,18 @@
+package com.app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+}
