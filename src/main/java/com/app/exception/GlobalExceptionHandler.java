540df773273e1329e863dc0120f4d7ebd362e4bc
Martin Zalazar <martinzetav@gmail.com>
2025-02-27 09:36:06 -0300

    se creo carpeta con exceptions personalizadas

---
+++ src/main/java/com/app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.app.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> processResourceNotFoundException(ResourceNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+}
---
+++ src/main/java/com/app/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.app.exception;
+
+public class ResourceNotFoundException extends Exception{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
