529c4219923c39f14dd7557149c1994372f5c1a5
Martin Zalazar <martinzetav@gmail.com>
2025-02-28 10:27:31 -0300

    se renombro request mapping de doctor a doctors

--- src/main/java/com/app/controller/DoctorController.java
+++ src/main/java/com/app/controller/DoctorController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/doctor")
+@RequestMapping("/doctors")
 @RequiredArgsConstructor
 public class DoctorController {
 
