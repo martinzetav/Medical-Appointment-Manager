e8435b3fdd426ef0d91dc7597eb8698d49967b95
Martin Zalazar <martinzetav@gmail.com>
2025-02-28 11:45:46 -0300

    se agregaron metodos para la busqueda de turnos

--- src/main/java/com/app/repository/IAppointmentRepository.java
+++ src/main/java/com/app/repository/IAppointmentRepository.java
@@ -4,6 +4,10 @@ import com.app.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface IAppointmentRepository extends JpaRepository<Appointment, Long> {
+    Set<Appointment> findByPatientId(Long patientId);
+    Set<Appointment> findByDoctorId(Long doctorId);
 }
