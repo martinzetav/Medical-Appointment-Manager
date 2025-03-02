c80dcde445664a43917b51af3f274e426d509404
Martin Zalazar <martinzetav@gmail.com>
2025-03-02 19:39:15 -0300

    se creo dto para transferir datos de Patient

---
+++ src/main/java/com/app/dto/PatientDTO.java
@@ -0,0 +1,21 @@
+package com.app.dto;
+
+import com.app.model.Address;
+import com.app.model.Contact;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+public class PatientDTO {
+    private Long id;
+    private String name;
+    private String lastName;
+    private Contact contact;
+    private Address address;
+    private Set<PatientAppointmentDTO> appointments;
+}
