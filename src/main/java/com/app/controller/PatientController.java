ec868a4f2bcff889a351f04787e76a8de4e8a607
Martin Zalazar <martinzetav@gmail.com>
2025-03-04 20:00:37 -0300

    se agregaro autorizaciones a los endpoints de Patient

--- src/main/java/com/app/controller/PatientController.java
+++ src/main/java/com/app/controller/PatientController.java
@@ -8,6 +8,7 @@ import com.app.model.Patient;
 import com.app.service.interfaces.IPatientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,11 +22,13 @@ public class PatientController {
 
     private final IPatientService patientService;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SECRETARY')")
     @GetMapping
     public ResponseEntity<List<PatientDTO>> findAll(){
         return ResponseEntity.ok(patientService.findAll());
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SECRETARY')")
     @GetMapping("/{id}")
     public ResponseEntity<PatientDTO> findById(@PathVariable Long id) throws ResourceNotFoundException {
         Optional<PatientDTO> requiredPatient = patientService.findById(id);
@@ -38,11 +41,13 @@ public class PatientController {
         return response;
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SECRETARY')")
     @PostMapping
     public ResponseEntity<PatientDTO> save(@RequestBody Patient patient) throws DuplicateResourceException {
         return ResponseEntity.ok(patientService.save(patient));
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SECRETARY')")
     @PutMapping("/{id}")
     public ResponseEntity<PatientDTO> update(@PathVariable Long id, @RequestBody Patient patient) throws ResourceNotFoundException {
         Optional<PatientDTO> patientToLookFor = patientService.findById(id);
@@ -55,17 +60,20 @@ public class PatientController {
         return response;
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) throws ResourceNotFoundException {
         patientService.delete(id);
         return ResponseEntity.ok("Patient with id " + id + " successfully removed.");
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SECRETARY', 'ROLE_USER')")
     @GetMapping("/{id}/appointments")
     public ResponseEntity<Set<PatientAppointmentDTO>> findAppointmentsByPatientId(@PathVariable Long id) throws ResourceNotFoundException {
         return ResponseEntity.ok(patientService.findAppointmentsByPatientId(id));
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SECRETARY')")
     @GetMapping("/search")
     public ResponseEntity<PatientDTO> findPatientByDni(@RequestParam String dni) throws ResourceNotFoundException {
         Optional<PatientDTO> requiredPatient = patientService.findPatientByDni(dni);
